2a0ff35de437b2c51bb17180f551eed05544917e
Tsz Tin Vivian Soo <tsztin0217@gmail.com>
2026-01-23 02:08:11 -0500

    implement ItemForm component and integrate with App for adding wish items

--- wishtracker/src/App.vue
+++ wishtracker/src/App.vue
@@ -1,12 +1,31 @@
 <script setup>
+import { ref } from 'vue'
+import { useAuth } from '@/composables/useAuth'
 import Login from './components/Login.vue'
+import ItemForm from './components/ItemForm.vue'
+
+const { isAuthenticated } = useAuth()
+const showForm = ref(false)
+const openForm = () => { showForm.value = true }
+const closeForm = () => { showForm.value = false }
+
+
 </script>
+
 
 <template>
   <main>
     <Login />
+    <div v-if="isAuthenticated">
+      <button v-if="!showForm" @click="openForm">
+        Add a Wish item
+      </button>
+      <ItemForm v-if="showForm" @close="showForm = false" />
+    </div>
+
   </main>
 </template>
+
 
 <style scoped>
 main {
---
+++ wishtracker/src/components/ItemForm.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from 'vue'
+
+const header = ref('Add a Wish Item')
+
+const emit = defineEmits(['close'])
+
+const cancel = () => {
+  emit('close')
+}
+
+</script>
+
+<template>
+    <h1>{{  header  }}</h1>
+    <input type="text" placeholder="Paste item link here!">
+    <button>Submit</button>
+    <button @click="cancel">Cancel</button>
+</template>
